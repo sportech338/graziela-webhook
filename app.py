cf963b0acf2e58136acefba8cc8c33660a2c8ac7
sportech338 <sportechofc01@gmail.com>
2025-04-23 14:10:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -257,4 +257,4 @@ def webhook():
     return make_response(jsonify({"payload": {"resposta": reply}}), 200)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
